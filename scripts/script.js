80f0d9b981744853ce839d4642201174b512b2b5
riaurko <riaurko@gmail.com>
2025-03-02 22:23:42 +0600

    Updated Home Page and Started Developing Blog Page

--- scripts/script.js
+++ scripts/script.js
@@ -10,46 +10,50 @@ const thisYear = new Date().getFullYear();
 const todayDateEl = document.querySelectorAll("#today-date > div > h4 span")[1];
 const todayDayEl = document.querySelector("#today-date > div > h4 span");
 todayDayEl.innerText = todayDay;
-todayDateEl.innerText = `${thisMonth} ${todayDate} ${thisYear}`;
+todayDateEl.innerText = `${todayDate} ${thisMonth} ${thisYear}`;
 
 
-//! changeTaskCounts() to decrease 'Assigned Task's Count' and increase 'Completed Task's Count'
-function changeTaskCounts() {
-    const assignedTasks = document.querySelector("#assigned-tasks > div > h3");
-    const completedTasks = document.querySelector("#completed-tasks > h4");
-    assignedTasks.innerText = `0${parseInt(assignedTasks.innerText) - 1}`;
-    completedTasks.innerText = parseInt(completedTasks.innerText) + 1;
-}
 
-//! disableCompleteBtn() to disable the 'Complete' Button in a Task Card
-function disableCompleteBtn(completeBtn) {
-    completeBtn.classList.remove('shadow', 'shadow-primary', 'hover:shadow-md', 'hover:shadow-blue-600', 'active:shadow-none', 'transition-shadow');
-    completeBtn.classList.add('bg-opacity-50');
-    completeBtn.classList.add('text-opacity-70');
-    completeBtn.setAttribute('disabled', null);
-}
-
-//! addActivity() to add a Dynamic Activity Box in Activity Log
-function addActivity(taskCard, clickTime) {
-    const taskTitle = taskCard.getElementsByClassName('title')[0].innerText;
-    const activityContainer = document.getElementById('activities');
-    const activity = document.createElement('div');
-    activity.classList.add('p-3', 'rounded-md', 'bg-[#E0F0FF]');
-    activity.innerText = `You've completed the task '${taskTitle}' at ${clickTime}`;
-    activityContainer.appendChild(activity)
-}
-
-
+let taskCount = 0;
 //! Adding EventListeners to Complete Task Buttons
-const taskCards = document.getElementsByClassName('task-card');
-for (let i = 0; i < 3; i++) {
-    let currentMeridiem;
-    if (new Date().getHours() < 12)
-        currentMeridiem = 'AM';
-    else
-        currentMeridiem = 'PM';
-    const instantTime = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} ${currentMeridiem}`;
-    taskCards[i].addEventListener('click', changeTaskCounts);
-    taskCards[i].addEventListener('click', () => disableCompleteBtn(taskCards[i].getElementsByClassName('complete-btn')[0]));
-    taskCards[i].addEventListener('click', () => addActivity(taskCards[i], instantTime));
-}
+document.getElementById('dashboard-tasks').addEventListener('click', function(event) {
+    if (event.target.classList.contains('complete-btn')) {
+        const datetime = new Date();
+        let meridiem;
+        if (datetime.getHours() < 12)
+            meridiem = 'AM';
+        else
+            meridiem = 'PM';
+        const currentTime = `${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()} ${meridiem}`;
+        const completeBtn = event.target;
+        const taskCard = completeBtn.closest('.task-card');
+        const taskTitle = taskCard.querySelector('.title').innerText;
+        taskCount++;
+        //! Showing Alert
+        window.alert("Board updated successfully!");
+        if (taskCount === 6)
+            window.alert("Congrats! You've completed all the Assigned Tasks.")
+        //! Changing Tasks Counts
+        const assignedTasks = document.querySelector("#assigned-tasks > div > h3");
+        const completedTasks = document.querySelector("#completed-tasks > h4");
+        assignedTasks.innerText = `0${parseInt(assignedTasks.innerText) - 1}`;
+        completedTasks.innerText = parseInt(completedTasks.innerText) + 1;
+        //! Disabling Complete Button
+        completeBtn.classList.remove('shadow', 'shadow-primary', 'hover:shadow-md', 'hover:shadow-blue-600', 'active:shadow-none', 'transition-shadow');
+        completeBtn.classList.add('bg-opacity-50', 'text-opacity-70');
+        completeBtn.setAttribute('disabled', null);
+        //! Adding a Activity in Activity Log
+        const activityContainer = document.getElementById('activities');
+        const activity = document.createElement('div');
+        activity.classList.add('p-3', 'rounded-md', 'bg-[#E0F0FF]');
+        activity.innerText = `You've completed the task '${taskTitle}' at ${currentTime}`;
+        activityContainer.appendChild(activity);
+    }
+});
+
+
+//! Adding EventListener in Clear Activity History Button
+document.querySelector("#activity-log-header > button").addEventListener('click', function(event) {
+    const clearBtn = event.target;
+    clearBtn.parentNode.parentNode.querySelector('#activities').innerHTML = null;
+});
